bd57db3ca6676d7d9902e33ebe726b585cf47c30
meenaFogah <yasmeenfoqha2001@gmail.com>
2020-12-10 02:01:53 -0800

    home trick components are ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import Nav from './components/Home/Nav';
+import Header from './components/Home/Header';
+import Footer from './components/Home/Footer';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+
 import './App.css';
+import Trick from './components/Trick/trickItem'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <BrowserRouter>
+
+            <Nav />
+            <Header/>
+            <Trick/>
+            <Footer/>
+          
+          </BrowserRouter>
+        </div>
     </div>
   );
 }
